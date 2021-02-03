e2454044941c2ca6bad79b3315a0a6e9732a7072
Дмитрий Савельев <mrpresident.evmount@gmail.com>
2021-02-03 18:37:42 +0300

    Work in the university and a little bit in the dormitory. Without try-catch-finally. But with working buttons and interface

---
+++ Лаба1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Лаба1
+{
+
+    public partial class Form1 : Form
+    {
+        public static string answer = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e) //не рабтает
+        {
+            foreach (var item in this.Controls) //обходим все элементы формы
+            {
+                if (item is Button) // проверяем, что это кнопка
+                {
+                    ((Button)item).Click += CommonBtn_Click; //приводим к типу и устанавливаем обработчик события  
+                }
+            }
+
+        }
+        private void CommonBtn_Click(object sender, EventArgs e)
+        {
+            Button but = sender as Button;
+            Calculation calc = new Calculation();
+
+            string str = but.Text;
+            if (str == "CE")
+            {
+                textBox1.Text = calc.Clear();
+            }
+            else if (str == "Del")
+            {
+                textBox1.Text = calc.Delete(textBox1.Text);
+            }
+            else if (str == "Ans")
+            {
+                textBox1.Text = calc.Answer();
+            }
+            else if (str == "=")
+            {
+                if (textBox1.Text.Contains("%"))
+                {
+                    textBox1.Text = calc.Remainder(textBox1.Text);
+                }
+                else if (textBox1.Text.Contains("Div"))
+                {
+                    textBox1.Text = calc.WholePart(textBox1.Text);
+                }
+                else
+                {
+                    textBox1.Text = calc.DefaultEqually(textBox1.Text);
+                }
+            }
+            else textBox1.Text = calc.BaseMathOperations(textBox1.Text, str);
+        }
+
+
+    }
+}
---
+++ Лаба1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+
+//Сложение, вычитание, деление, умножение получение остатка от
+//деления, получение целой части деления, очистка. Добавьте
+//операции хранения и извлечения значения в памяти.
+namespace Лаба1
+{
+    interface ICalculations
+    {
+        string Clear();
+        string Delete(string str);
+        string BaseMathOperations(string str, string operation);
+        string Answer();
+        string Remainder(string str);
+        string WholePart(string str);
+        string DefaultEqually(string str);
+    }
+    class Calculation : ICalculations
+    {
+        public string Clear()
+        {
+            return "";
+        }
+        public string Delete(string str)
+        {
+            return str.Remove(str.Length - 1);
+        }
+        public string BaseMathOperations(string str, string op)
+        {
+            return str + op;
+        }
+        public string Answer()
+        {
+            return Form1.answer;
+        }
+        public string Remainder(string str)
+        {
+            string[] operands = str.Split('%');
+            Form1.answer = (Double.Parse(operands[0]) % Double.Parse(operands[1])).ToString();
+            return Form1.answer;
+        }
+        public string WholePart(string str)
+        {
+            try
+            {
+                string[] op = new string[2];
+                string[] operands = str.Split(new char[] { 'D', 'i', 'v' });
+                int i = 0;
+                foreach (string str4 in operands)
+                {
+                    if (str4 != "")
+                    {
+                        op[i] = str4;
+                        i++;
+                    }
+                }
+                if (i == 2)
+                {
+                    Form1.answer = Math.Truncate(Double.Parse(op[0]) / Double.Parse(op[1])).ToString();
+                    return Form1.answer;
+                }
+                else
+                    throw new Exception("Wrong Input!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return "";
+
+        }
+        public string DefaultEqually(string str)
+        {
+            string value = new DataTable().Compute(str, null).ToString();
+            Form1.answer = value;
+            return Form1.answer;
+
+        }
+    }
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
